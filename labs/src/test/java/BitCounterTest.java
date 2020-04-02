62a30f13ebda1b0f69079cc1dc7b5ca2cd1a3900
PiotrMor <piotrmorawiecki97@gmail.com>
2020-04-02 17:53:15 +0200

    Part f) of lab2

--- labs/src/test/java/BitCounterTest.java
+++ labs/src/test/java/BitCounterTest.java
@@ -1,59 +1,66 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.NumberFormatException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class BitCounterTest {
 
-    private BitCounter generator;
+    private BitCounter bitCounter;
 
     @BeforeEach
     void beforeEach() {
-        generator = new BitCounter();
+        bitCounter = new BitCounter();
     }
 
     @Test
     void noOfBits1Test() {
-        assertThat(generator.noOfBits1("4"))
+        assertThat(bitCounter.noOfBits1("4"))
                 .isEqualTo(1);
 
-        assertThat(generator.noOfBits1("123"))
+        assertThat(bitCounter.noOfBits1("123"))
                 .isEqualTo(6);
     }
 
     @Test
     void noOfBits1EmptyStringTest() {
-        assertThat(generator.noOfBits1(""))
+        assertThat(bitCounter.noOfBits1(""))
                 .isEqualTo(0);
     }
 
     @Test
     void noOfBits1IncorrectParameterTest() {
-        assertThrows(RuntimeException.class, () -> generator.noOfBits1("123123"));
-        assertThrows(RuntimeException.class, () -> generator.noOfBits1("-5"));
+        assertThrows(RuntimeException.class, () -> bitCounter.noOfBits1("123123"));
+        assertThrows(RuntimeException.class, () -> bitCounter.noOfBits1("-5"));
     }
 
     @Test
     void noOfBits1MultipleNumbersTest() {
-        assertThat(generator.noOfBits1("3;5"))
+        assertThat(bitCounter.noOfBits1("3;5"))
                 .isEqualTo(4);
     }
 
     @Test
     void noOfBits1SpaceAndSemicolonDelimitersTest() {
-        assertThat(generator.noOfBits1("3;5  8"))
+        assertThat(bitCounter.noOfBits1("3;5  8"))
                 .isEqualTo(5);
     }
 
     @Test
     void noOfBits1WhiteSpacesDelimitersTest() {
-        assertThat(generator.noOfBits1("3 5     8"))
+        assertThat(bitCounter.noOfBits1("3 5     8"))
                 .isEqualTo(5);
 
-        assertThat(generator.noOfBits1("3;5\n8"))
+        assertThat(bitCounter.noOfBits1("3;5\n8"))
                 .isEqualTo(5);
+    }
+
+    @Test
+    void noOfBits1InvalidSeparatorTest() {
+        assertThrows(NumberFormatException.class, () -> bitCounter.noOfBits1("3:3:2:4"));
     }
 
 }
