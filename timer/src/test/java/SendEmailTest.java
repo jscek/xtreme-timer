537f57cc8bda908cd0e0268972fe55ba4379860f
domi-nika60 <dominikafurgala1@gmail.com>
2020-05-07 20:45:24 +0200

    Send report by mail

---
+++ timer/src/test/java/SendEmailTest.java
@@ -0,0 +1,18 @@
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.Test;
+import timer.SendEmail;
+
+public class SendEmailTest {
+
+    @Test
+    public void send() {
+
+        SendEmail newMail = new SendEmail();
+        org.junit.jupiter.api.Assertions.assertDoesNotThrow(
+                ()-> newMail.main(null)
+        );
+    }
+
+}
