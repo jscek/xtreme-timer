c14fb7f09beabc6068c95c4d026d74349f8ef86e
PiotrMor <piotrmorawiecki97@gmail.com>
2020-05-08 19:32:31 +0200

    Fix duration of timers

--- timer/src/main/java/timer/TimerRecord.java
+++ timer/src/main/java/timer/TimerRecord.java
@@ -4,79 +4,93 @@ import java.time.Duration;
 import java.time.Instant;
 
 public class TimerRecord {
-	private Long id;
-	private String projectName;
-	private Instant startTime;
-	private Instant stopTime;
-	private boolean isRunning = false;
-	private Duration duration;
+    private Long id;
+    private String projectName;
+    private Instant startTime;
+    private Instant stopTime;
+    private boolean isRunning = false;
+    private Duration duration;
 
-	public TimerRecord(Long id) {
-		this(id, "--");
-	}
+    public TimerRecord(Long id) {
+        this(id, "--");
+    }
 
-	public TimerRecord(Long id, String projectName) {
-		this.id = id;
-		this.projectName = projectName;
-	}
+    public TimerRecord(Long id, String projectName) {
+        this.id = id;
+        this.projectName = projectName;
+    }
 
-	public TimerRecord(Long id, String projectName, Instant startTime, Instant stopTime, boolean isRunning, Duration duration) {
-		this.id = id;
-		this.projectName = projectName;
-		this.startTime = startTime;
-		this.stopTime = stopTime;
-		this.isRunning = isRunning;
-		this.duration = duration;
-	}
+    public TimerRecord(Long id, String projectName, Instant startTime, Instant stopTime, boolean isRunning, Duration duration) {
+        this.id = id;
+        this.projectName = projectName;
+        this.startTime = startTime;
+        this.stopTime = stopTime;
+        this.isRunning = isRunning;
+        this.duration = duration;
+    }
 
-	public Instant getStartTime() {
-		return startTime;
-	}
+    public Instant getStartTime() {
+        return startTime;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void stopTimer() {
-		isRunning = false;
-		stopTime = Instant.now();
-		duration = getDuration();
-	}
+    public void stopTimer() {
+        duration = getDuration();
+        stopTime = Instant.now();
+        isRunning = false;
+    }
 
-	public Instant getStopTime() {
-		return stopTime;
+    public Instant getStopTime() {
+        return stopTime;
+    }
 
-	}
+    public Duration getDuration() {
+        if (isRunning) {
+            return getDurationWhenRunning();
+        } else {
+            return getDurationWhenStopped();
+        }
 
-	public Duration getDuration() {
-		if (startTime == null) {
-			return Duration.ZERO;
-		} else if (stopTime == null) {
-			return Duration.between(startTime, Instant.now());
-		} else if (duration != null) {
-			return duration.plus(Duration.between(startTime, stopTime));
-		} else {
-			return Duration.between(startTime, stopTime);
-		}
+    }
 
-	}
+    private Duration getDurationWhenStopped() {
+        if (startTime == null && duration == null) {
+            return Duration.ZERO;
+        } else {
+            return duration;
+        }
+    }
 
-	public boolean isRunning() {
-		return isRunning;
-	}
+    private Duration getDurationWhenRunning() {
+        Duration currentDuration = duration != null ? duration : Duration.ZERO;
 
-	public void resume() {
-		stopTime = null;
-		startTime = Instant.now();
-		isRunning = true;
-	}
+        if (stopTime != null) {
+            return currentDuration.plus(Duration.between(startTime, stopTime));
+        } else {
+            return currentDuration.plus(Duration.between(startTime, Instant.now()));
+        }
 
-	public void startTimer() {
-		startTime = Instant.now();
-		this.isRunning = true;
-	}
+    }
 
-	public String getProjectName() {
-		return projectName;
-	}
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void resume() {
+        stopTime = null;
+        startTime = Instant.now();
+        isRunning = true;
+    }
+
+    public void startTimer() {
+        startTime = Instant.now();
+        this.isRunning = true;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
 }
