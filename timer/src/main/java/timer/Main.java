517915489330d8354611b6e74a0cb1027e0b44f6
PiotrMor <piotrmorawiecki97@gmail.com>
2020-06-22 16:19:23 +0200

    Use abstraction in main

--- timer/src/main/java/timer/Main.java
+++ timer/src/main/java/timer/Main.java
@@ -1,11 +1,12 @@
 package timer;
 
 
+import timer.base.TimerApp;
 import timer.base.TimerCommandLineApp;
 
 public class Main {
 	public static void main(String[] args) {
-		TimerCommandLineApp timerApp = new TimerCommandLineApp();
+		TimerApp timerApp = new TimerCommandLineApp();
 		timerApp.start();
 		Runtime.getRuntime().exit(0);
 	}
