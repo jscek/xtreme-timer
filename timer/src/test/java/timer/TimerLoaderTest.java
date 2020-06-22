f73c5cb3f8f22e51d4f68548e29009e93d290d21
PiotrMor <piotrmorawiecki97@gmail.com>
2020-06-22 19:24:09 +0200

    Refactor

--- timer/src/test/java/timer/TimerLoaderTest.java
+++ timer/src/test/java/timer/TimerLoaderTest.java
@@ -28,21 +28,4 @@ class TimerLoaderTest {
         assertThat(loader.loadFromFile("")).isEqualTo(Collections.emptyList());
     }
 
-    @Test
-    public void parseSingleLineToTimerRecord() {
-        Long id = 1L;
-        String projectName = "test";
-        Instant startTime = Instant.parse("2020-04-20T08:00:00.00Z");
-        Instant stopTime = Instant.parse("2020-04-20T10:00:00.00Z");
-        boolean isRunning = false;
-        Duration duration = Duration.ofSeconds(7200);
-
-        TimerRecord originalRecord = new TimerRecord(id, projectName, startTime, startTime, stopTime, isRunning, duration);
-
-        String line = id + ";" + projectName + ";" + startTime + ";" + stopTime + ";" + isRunning + ";" + duration.getSeconds();
-
-        TimerRecord parsedRecord = loader.convertStringToTimerRecord(line);
-
-        assertThat(originalRecord).isEqualToComparingFieldByField(parsedRecord);
-    }
 }
